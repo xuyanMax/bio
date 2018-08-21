c9c3ac46284674105b19507ab6cd076419b622e2
xuyanMax <418780414@qq.com>
2018-08-21 23:10:40 +0800

    更新公众号自定义菜单

--- src/main/java/com/wechat/thread/MenuThread.java
+++ src/main/java/com/wechat/thread/MenuThread.java
@@ -5,11 +5,7 @@ import com.wechat.model.button.*;
 import com.wechat.utils.AccessTokenUtil;
 import com.wechat.utils.WeChatUtils;
 import org.apache.log4j.Logger;
-/*
-* 服务号
-* appid: wxb92b6517e66c5eda
-* secret: 7953a4803072b35c8e41ed27933f0ecb
-* */
+
 public class MenuThread implements Runnable {
     private static final String appID = "wxb92b6517e66c5eda";//wx0f81f68f813bd68d
     private static final String appSecret = "7953a4803072b35c8e41ed27933f0ecb";//c9d7f54ec1d0642d187141636ba69af2
@@ -31,87 +27,6 @@ public class MenuThread implements Runnable {
         }
     }
     public static Menu getMenu(){
-        CommonButton btn11 = new CommonButton();
-        btn11.setName("天气预报");
-        btn11.setType("click");
-        btn11.setKey("11");
-
-        CommonButton btn12 = new CommonButton();
-        btn12.setName("公交查询");
-        btn12.setType("click");
-        btn12.setKey("12");
-
-        CommonButton btn13 = new CommonButton();
-        btn13.setName("周边搜索");
-        btn13.setType("click");
-        btn13.setKey("13");
-
-        CommonButton btn14 = new CommonButton();
-        btn14.setName("历史上的今天");
-        btn14.setType("click");
-        btn14.setKey("14");
-
-        CommonButton btn21 = new CommonButton();
-        btn21.setName("歌曲点播");
-        btn21.setType("click");
-        btn21.setKey("21");
-
-        CommonButton btn22 = new CommonButton();
-        btn22.setName("经典游戏");
-        btn22.setType("click");
-        btn22.setKey("22");
-
-        CommonButton btn23 = new CommonButton();
-        btn23.setName("电台");
-        btn23.setType("click");
-        btn23.setKey("23");
-
-        CommonButton btn24 = new CommonButton();
-        btn24.setName("人脸识别");
-        btn24.setType("click");
-        btn24.setKey("24");
-
-        CommonButton btn25 = new CommonButton();
-        btn25.setName("聊天唠嗑");
-        btn25.setType("click");
-        btn25.setKey("25");
-
-        CommonButton btn31 = new CommonButton();
-        btn31.setName("Q友圈");
-        btn31.setType("click");
-        btn31.setKey("31");
-
-      /*  CommonButton btn32 = new CommonButton();
-        btn32.setName("电影排行榜");
-        btn32.setType("click");
-        btn32.setKey("32");*/
-
-        CommonButton btn33 = new CommonButton();
-        btn33.setName("幽默笑话");
-        btn33.setType("click");
-        btn33.setKey("33");
-        CommonButton btn35 = new CommonButton();
-        btn35.setName("关于我们");
-        btn35.setType("click");
-        btn35.setKey("35");
-
-        ViewButton btn32 = new ViewButton();
-        btn32.setName("使用帮助");
-        btn32.setType("view");
-        btn32.setUrl("http://liufeng.gotoip2.com/xiaoqrobot/help.jsp");
-
-        ParentButton mainBtn1 = new ParentButton();
-        mainBtn1.setName("生活助手");
-        mainBtn1.setSub_button(new CommonButton[] { btn11, btn12, btn13, btn14 });
-
-        ParentButton mainBtn2 = new ParentButton();
-        mainBtn2.setName("休闲驿站");
-        mainBtn2.setSub_button(new CommonButton[] { btn21, btn22, btn23, btn24, btn25 });
-
-        ParentButton mainBtn3 = new ParentButton();
-        mainBtn3.setName("更多体验");
-        mainBtn3.setSub_button(new Button[] { btn31, btn35, btn33, btn32});
-
         /**
          * 这是公众号xiaoqrobot目前的菜单结构，每个一级菜单都有二级菜单项<br>
          *
@@ -120,7 +35,22 @@ public class MenuThread implements Runnable {
          * menu.setButton(new Button[] { mainBtn1, mainBtn2, btn33 });
          */
         Menu menu = new Menu();
-        menu.setButton(new Button[] { mainBtn1, mainBtn2, mainBtn3 });
+        ViewButton left = new ViewButton();
+        left.setType("view");
+        left.setName("中心简介");
+        left.setUrl("http://www.chgc.sh.cn/page60");
+
+        ViewButton center = new ViewButton();
+        center.setName("科研合作Flup");
+        center.setType("view");
+        center.setUrl("population.chgc.sh.cn");
+
+        ViewButton right = new ViewButton();
+        right.setName("新闻动态");
+        right.setUrl("http://www.chgc.sh.cn/page104");
+        right.setType("view");
+
+        menu.setButton(new Button[] {left, center, right});
 
         return menu;
     }
